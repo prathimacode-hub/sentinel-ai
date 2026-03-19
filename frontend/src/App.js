2c529e96b6fd495b9b95d44fc6640afc5e06e5c5
Prathima Kadari <kadariprathima4@gmail.com>
2026-03-19 12:06:56 +0530

    Created App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./dashboard/Dashboard";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
